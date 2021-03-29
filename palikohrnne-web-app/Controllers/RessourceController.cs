89c57c6a7dd3f6c042100b4453ce0728a7d3da7e
Valentin <valentin.beaury@fremach.com>
2021-03-30 00:06:29 +0200

    lister les ressources front premier jet

---
+++ palikohrnne-web-app/Controllers/RessourceController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace palikohrnne_web_app.Controllers
+{
+    public class RessourceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
