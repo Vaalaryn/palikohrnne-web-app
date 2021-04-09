91d2157a81fded426ec07410ed377213863a4307
Valentin <beaury.valentin@gmail.com>
2021-04-09 19:44:40 +0200

    Liste des ressources admin

--- palikohrnne-web-app/Controllers/BackOfficeController.cs
+++ palikohrnne-web-app/Controllers/BackOfficeController.cs
@@ -18,9 +18,119 @@ namespace palikohrnne_web_app.Controllers
             _cubesService = cubesService;
         }
 
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<IActionResult> ListeRessource(string order, FiltresModelRessources filtres)
+        {
+            IEnumerable<Ressource> ressources = await _cubesService.GetAllRessources();
+            IEnumerable<TypeRelation> typesRelations = await _cubesService.GetAllTypeRelations();
+            IEnumerable<Tag> tags = await _cubesService.GetAllTags();
+
+            if (string.IsNullOrEmpty(filtres.AnswersFilter))
+            {
+                filtres.AnswersFilter = "all-answers";
+            }
+
+            //Filtres -------------------------------------
+            //Filtre des réponses
+            switch (filtres.AnswersFilter)
+            {
+                case "no-answers":
+                    {
+                        ressources = ressources.Where(x => x.Commentaires.Count == 0);
+                        break;
+                    }
+                case "only-answers":
+                    {
+                        ressources = ressources.Where(x => x.Commentaires.Count > 0);
+                        break;
+                    }
+                default:
+                    {
+                        break;
+                    }
+            }
+            //Filtre ancienneté
+            switch (filtres.AgeMax)
+            {
+                case "lastWeek":
+                    {
+                        var monday = DateTime.Today.AddDays(-(int)DateTime.Today.DayOfWeek + (int)DayOfWeek.Monday);
+                        ressources = ressources.Where(x => x.CreatedAt >= monday);
+                        break;
+                    }
+                case "lastMonth":
+                    {
+                        ressources = ressources.Where(x => x.CreatedAt.Month == DateTime.Now.Month);
+                        break;
+                    }
+                case "lastYear":
+                    {
+                        ressources = ressources.Where(x => x.CreatedAt.Year == DateTime.Now.Year);
+                        break;
+                    }
+                default:
+                    {
+                        break;
+                    }
+            }
+            //Filtre cercle social
+            if ((filtres.TypeRelationID != null) && (filtres.TypeRelationID.Any()))
+            {
+                ressources = ressources.Where(x => filtres.TypeRelationID.Contains(x.TypeRelationID));
+            }
+            else
+            {
+            }
+            //Filtre tags
+            if ((filtres.TagsID != null) && (filtres.TagsID.Any()))
+            {
+                ressources = ressources.Where(x => x.Tags.Select(x => x.ID).Where(x => filtres.TagsID.Contains(x)).Any());
+            }
+            //FIN Filtres -------------------------------------
+
+
+            //Tri
+            switch (order)
+            {
+                case "recents":
+                    {
+                        ressources = ressources.OrderByDescending(x => x.CreatedAt);
+                        break;
+                    }
+                case "reponsesDesc":
+                    {
+                        ressources = ressources.OrderByDescending(x => x.Commentaires.Count);
+                        break;
+                    }
+                case "reponsesAsc":
+                    {
+                        ressources = ressources.OrderBy(x => x.Commentaires.Count);
+                        break;
+                    }
+                default:
+                    {
+                        ressources = ressources.OrderBy(x => x.CreatedAt);
+                        break;
+                    }
+            }
+
+            //Cache
+            ViewBag.Order = order;
+            ViewBag.TypesRelations = typesRelations;
+            ViewBag.Tags = new SelectList(tags, "ID", "Nom");
+
+            //Création du model
+            ListeRessourceModel model = new ListeRessourceModel
+            {
+                Ressources = ressources.Where(x => x.ValidationAdmin == true).ToList(),
+                Filtres = filtres
+            };
+            return View(model);
         }
 
         public async Task<IActionResult> ListCitoyens()
